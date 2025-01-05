c90848b3f47da2c356f6c59cc193afb3cfbb6e23
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-01-04 21:04:07 -0500

    Cleanup Codebase
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

---
+++ src/main/kotlin/dev/ixor/superuserkamtheme/KAMThemeManager.kt
@@ -0,0 +1,14 @@
+package dev.ixor.superuserkamtheme
+
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.extensions.PluginId
+import java.util.*
+
+class KAMThemeManager {
+    companion object {
+        private const val PLUGIN_ID : String = "dev.ixor.superuserkam-theme"
+
+        val currentVersion : String
+            get() = Objects.requireNonNull(PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID))?.version ?: "")
+    }
+}
